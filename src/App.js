03651609d78f9d2d71988d5bee4fee0a67ac8fb4
Ahmed <atomcgarry@gmail.com>
2022-01-12 19:49:45 +0000

    basic layout

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <div className="daniel"></div>
+        <div className="walters"></div>
+        <div className="kiera"></div>
+        <div className="jean"></div>
+        <div className="patrick"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
